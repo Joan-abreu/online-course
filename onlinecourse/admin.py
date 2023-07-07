e576433f194b6ed66a142faf75295c5d5181ec3a
Joan-abreu <joan.abreu2007@gmail.com>
2023-07-07 16:08:37 -0400

    Add Question and Choice models

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
-
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -19,12 +24,22 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'order']
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'grade')
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'is_correct')
 
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
